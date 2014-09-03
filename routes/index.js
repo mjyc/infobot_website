22c6bde20b4b073dec685fd6270ec776de532e5c
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-09-02 17:37:35 -0700

    dropping map page for now

--- routes/index.js
+++ routes/index.js
@@ -1,92 +1,105 @@
+// =====================================================================
+// Requires.
+// =====================================================================
+
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-// Helper Function =====================================================
 
-// Route middleware to ensure user is logged in
+// =====================================================================
+// Helper Function
+// =====================================================================
+
+// Route middleware to ensure user is logged in.
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated())
+    return next();
 
-    res.redirect('/');
+  res.redirect('/');
 }
 
-// Routes ==============================================================
 
-// Login
+// =====================================================================
+// Routes
+// =====================================================================
+
+// Login.
 router.get('/', function(req, res) {
   if (req.isAuthenticated())
-    res.redirect('/home')
+  res.redirect('/home')
   else
-    res.render('login.jade', {
-      isAuthenticated: req.isAuthenticated()
-    });
+  res.render('login.jade', {
+    isAuthenticated: req.isAuthenticated()
+  });
 });
 
-// Home
+// Home.
 router.get('/home', isLoggedIn, function(req, res) {
   res.render('home.jade', {
-    isAuthenticated: req.isAuthenticated(),
-    user: req.user
+  isAuthenticated: req.isAuthenticated(),
+  user: req.user
   });
 });
 
-// Map
-router.get('/map', isLoggedIn, function(req, res) {
-  res.render('map.jade', {
-    isAuthenticated: req.isAuthenticated(),
-    user: req.user
-  });
-});
-
-// Logout
+// Logout.
 router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
-// Authenticate (First Login) ==========================================
+
+// =====================================================================
+// Authenticate (First Login)
+// =====================================================================
 
 // Google
 
-// Send to google to do the authentication
+// Send to google to do the authentication.
 router.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
+  scope: ['profile', 'email']
 }));
 
-// The callback after google has authenticated the user
+// The callback after google has authenticated the user.
 router.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/home',
-        failureRedirect: '/'
-    }));
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/'
+  }));
 
-// Authorize (Already Logged In / Connecting Other Social Account) =====
+
+// =====================================================================
+// Authorize (Already Logged In / Connecting Other Social Account)
+// =====================================================================
 
 // Google
 
-// Send to google to do the authentication
+// Send to google to do the authentication.
 router.get('/connect/google', passport.authorize('google', {
-    scope: ['profile', 'email']
+  scope: ['profile', 'email']
 }));
 
-// The callback after google has authorized the user
+// The callback after google has authorized the user.
 router.get('/connect/google/callback',
-    passport.authorize('google', {
-        successRedirect: '/home',
-        failureRedirect: '/'
-    }));
+  passport.authorize('google', {
+    successRedirect: '/home',
+    failureRedirect: '/'
+  }));
 
-// Unlink Accounts =====================================================
+
+// =====================================================================
+// Unlink Accounts
+// =====================================================================
 
 // Google
+
 router.get('/unlink/google', isLoggedIn, function(req, res) {
-    var user = req.user;
-    user.google.token = undefined;
-    user.save(function(err) {
-        res.redirect('/home');
-    });
+  var user = req.user;
+  user.google.token = undefined;
+  user.save(function(err) {
+    res.redirect('/home');
+  });
 });
+
 
 module.exports = router;
