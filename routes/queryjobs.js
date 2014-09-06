ae7d5de9bdf177b21476ac5bc64d2f283ac1dcb7
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-09-06 09:06:10 -0700

    routing up to date

--- routes/queryjobs.js
+++ routes/queryjobs.js
@@ -5,67 +5,26 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/user');
-var QueryJob = require('../models/queryjob');
-
 
 // =====================================================================
 // Routes
 // =====================================================================
 
-// POST to adduser.
-router.post('/addqueryjob', function(req, res) {
-
-  console.log(req.user._id);
-  console.log(req.body);
-
-  var newQueryJob = new QueryJob();
-  newQueryJob.user_id = req.user._id;
+// POST to addqueryjob.
+router.post('/addqueryjob', function(req, res, next) {
+  var db = req.db;
+  var newQueryJob = {};
+  newQueryJob.user_id = req.body.user_id;
+  newQueryJob.timeissued = req.body.timeissued;
   newQueryJob.typed_cmd = req.body.typed_cmd;
   newQueryJob.notification_sms = req.body.sms_notification;
   newQueryJob.notification_email = req.body.email_notification;
-  console.log(newQueryJob);
+  newQueryJob.deadline = req.body.deadline;
 
-  newQueryJob.save(function(err) {
-    res.send(
-      (err === null) ? {msg: ''} : {msg: err}
-    );
+  db.collection('queryjobs').insert(req.body, function(err, result) {
+      if (err) { return next(err); }
+      res.send(result);
   });
-
-  // // if the user is not already logged in:
-  // if (!req.user) {
-  //   User.findOne({ 'local.email' :  email }, function(err, user) {
-  //     // if there are any errors, return the error
-  //     if (err)
-  //       return done(err);
-
-  //     // check to see if theres already a user with that email
-  //     if (user) {
-  //       return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-  //     } else {
-
-  //       // create the user
-  //       var newUser            = new User();
-
-  //       newUser.local.email    = email;
-  //       newUser.local.password = newUser.generateHash(password);
-
-  //       newUser.save(function(err) {
-  //         if (err)
-  //           return done(err);
-
-  //         return done(null, newUser);
-  //       });
-  //     }
-
-  //   });
-
-  // var db = req.db;
-  // db.collection('queryjob').insert(req.body, function(err, result){
-  //     res.send(
-  //         (err === null) ? { msg: '' } : { msg: err }
-  //     );
-  // });
 });
 
 
