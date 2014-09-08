a41adb6c843d4fdcd2daaa21d9f3a63b81642579
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-09-08 10:55:26 -0700

    Formatting update and dealing with ROS disconnection

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// Requires.
+// =====================================================================
+// Requires
+// =====================================================================
+
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -9,23 +12,44 @@ var mongo = require('mongoskin');
 var passport = require('passport');
 var flash = require('connect-flash');
 var session = require('express-session');
+var ROSLIB = require('roslib');
 
 // Locals.
 var routes = require('./routes/index');
 var queryjobs = require('./routes/queryjobs');
 var configDB = require('./config/database.js');
 var sessionDB = require('./config/session.js');
-var configTest = require('./config/test.js');
 
 
-// DB and passport setups.
-var dbUrl = configDB.url;
-if (process.env.TEST) {
-  dbUrl = configTest.dbUrl;
-}
+// =====================================================================
+// Setups
+// =====================================================================
+
+// ROS setup.
+var ros = new ROSLIB.Ros({ url : 'ws://localhost:9090' });
+ros.connection = false;
+ros.on('connection', function() {
+  ros.connection = true;
+  console.log('Connected to websocket server.');
+});
+ros.on('error', function(error) {
+  ros.connection = false;
+  console.log('Error connecting to websocket server: ', error);
+});
+ros.on('close', function() {
+  ros.connection = false;
+  console.log('Connection to websocket server closed.');
+});
+
+// DB setup.
+var dbUrl = (process.env.TEST) ? configDB.urlTest : configDB.url;
 var db = mongo.db(dbUrl, {native_parser:true});  // connect to db
+
+// Passport setup.
 require('./config/passport')(passport, db);  // load passport
 
+
+// Express
 
 var app = express();
 
@@ -46,10 +70,11 @@ app.use(passport.session());  // persistent login sessions
 app.use(flash());  // use connect-flash for flash messages stored in
 //   session
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+// Make our ros and db accessible to our router
+app.use(function(req, res, next) {
+  req.ros = ros;
+  req.db = db;
+  next();
 });
 
 app.use('/', routes);
@@ -57,9 +82,9 @@ app.use('/queryjobs', queryjobs);
 
 // Catch 404 and forward to error handler.
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // Error handlers
@@ -67,23 +92,23 @@ app.use(function(req, res, next) {
 // Development error handler.
 // Will print stacktrace.
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // Production error handler.
 // No stacktraces leaked to user.
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
