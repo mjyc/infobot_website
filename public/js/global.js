116e6560e5634a76e9637810796b3507e47c316a
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-09-10 07:55:19 -0700

    check in before removal

--- public/js/global.js
+++ public/js/global.js
@@ -4,126 +4,169 @@
 
 $(document).ready(function() {
 
-  $('#btnimgresult1').click(function() {
-    $('#imgresult1').toggle();
-  });
+  // // ROS Setups.
+  // var ros = new ROSLIB.Ros({
+  //   url : 'ws://localhost:9090'
+  // });
 
-  // $('#imgresult1').hover(function() {
-  //     $(this).css("cursor", "pointer");
-  //     $(this).animate({
-  //         width: "300px",
-  //         height: "280px"
-  //     });
+  // ros.on('connection', function() {
+  //   console.log('Connected to websocket server.');
+  // });
 
-  // }, function() {
-  //     $(this).animate({
-  //         width: "128px",
-  //         height: "128px"
-  //     });
+  // ros.on('error', function(error) {
+  //   // TODO: redirect to other page.
+  //   console.log('Error connecting to websocket server: ', error);
+  //   alert('There is some problem with DUB-E... Please visit us later.');
+  // });
+
+  // ros.on('close', function() {
+  //   // TODO: redirect to other page.
+  //   console.log('Connection to websocket server closed.');
+  //   alert('There is some problem with DUB-E... Please visit us later.');
+  // });
+
+  // // Populate query feeds.
+  // populateFeed(qFeedPage);
+
+  // // Set up buttons.
+  // $('#Home').on('click', 'a', function() {
+  //   qFeedPage = false;
+  //   populateFeed(qFeedPage);
+  // });
+
+  // $('#QFeed').on('click', 'a', function() {
+  //   qFeedPage = true;
+  //   populateFeed(qFeedPage);
+  // });
+
+  // // Submit Question button click.
+  // $('#btnSubmitQuestion').on('click', submitQuestion);
+
+  // $(window).scroll(function() {
+  //   var wintop = $(window).scrollTop();
+  //   var docheight = $(document).height();
+  //   var winheight = $(window).height();
+  //   var scrolltrigger = 0.95;
+
+  //   if  ((wintop/(docheight-winheight)) > scrolltrigger) {
+  //     populateFeed(qFeedPage, lastDate);
+  //   }
 
   // });
 
-  // $('#submitQuestion #inputDeadline').timepicker();
-
-  // Populate the user table on initial page load
-  // populateFeed();
-
-  // // Add User button click.
-  // $('#btnGetQuery').on('click', getQuery);
-
-  // // Add User button click.
-  // $('#btnPatrol').on('click', sendPatrol);
-
-  // // Add User button click.
-  // $('#btnCSEXXX').on('click', sendCSE4XX);
-
-  // // Add User button click.
-  // $('#btnSTART').on('click', sendSTART);
-
-  // // Username link click.
-  // $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
-
-  // Add User button click.
-  $('#btnSubmitQuestion').on('click', submitQuestion);
-
-  // // Delete User link click.
-  // $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 
+var Home = function() {
+  var that = this;
 
-// ROS
-var ros = new ROSLIB.Ros({
-  url : 'ws://localhost:9090'
-});
+  var populateFeed = function(data, callback) {
+    $.post('/queryjobs/listqueryjobs', data, function(results) {
+        var timeissued = null;
+        if (results.length > 1) {
+          timeissued = results[results.length-1].timeissued;
+        }
 
-ros.on('connection', function() {
-  console.log('Connected to websocket server.');
-});
+        var feedContent = '';
+        $.each(results, function(index, value){
+          console.log(index);
+          console.log(value);
+          // outData.feedContent += createFeed(this);
+        });
 
-ros.on('error', function(error) {
-  console.log('Error connecting to websocket server: ', error);
-});
+        callback({
+          'timeissued': timeissued,
+          'feedContent': feedContent
+        });
+    }, 'JSON');
+  };
 
-ros.on('close', function() {
-  console.log('Connection to websocket server closed.');
-});
-
-var testService = function(queryjob_id, timeissued, cb) {
-  var scheduleQueryJobClient = new ROSLIB.Service({
-    ros : ros,
-    name : '/schedule_query',
-    serviceType : 'sara_queryjob_manager/ScheduleQueryJob'
-  });
-
-  var request = new ROSLIB.ServiceRequest({
-    'queryjob_id' : queryjob_id,
-    'timeissued' : timeissued
-  });
-
-  scheduleQueryJobClient.callService(request, cb);
 };
+
+// Home.prototype.refreshFeed = function(data) {
+//   var that = this;
+
+//   var data = {
+//     'startDate': new Date(),
+//     'limit': LIMIT,
+//     'userOnly': data.userOnly
+//   };
+//   var callback = function(result) {
+//     $('#feedList').html(result.feedContent);
+//   };
+
+//   that.populateFeed(data, callback);
+// };
+
+
 
 // =====================================================================
 // Functions
 // =====================================================================
 
-// // Fill table with data
-// function populateTable() {
-
-//     // Empty content string
-//     var feedContent = '';
-
-//     // jQuery AJAX call for JSON
-//     $.getJSON('/queryjobs/listqueryjob', function( data ) {
-
-//         // Stick our user data array into a userlist variable in the global object
-//         userListData = data;
-
-//         // // For each item in our JSON, add a table row and cells to the content string
-//         // $.each(data, function(){
-//         //     feedContent += '<tr>';
-//         //     feedContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
-//         //     feedContent += '<td>' + this.email + '</td>';
-//         //     feedContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-//         //     feedContent += '</tr>';
-//         // });
-
-//         // // Inject the whole content string into our existing HTML table
-//         // $('#userList table tbody').html(tableContent);
-//     });
+// function formatAMPM(date) {
+//   var hours = date.getHours();
+//   var minutes = date.getMinutes();
+//   var ampm = hours >= 12 ? 'pm' : 'am';
+//   hours = hours % 12;
+//   hours = hours ? hours : 12; // the hour '0' should be '12'
+//   minutes = minutes < 10 ? '0'+minutes : minutes;
+//   var strTime = hours + ':' + minutes + ' ' + ampm;
+//   return strTime;
 // }
 
-// Add User.
+// function formatTodayYesterday(date) {
+//   var strTime = '';
+//   var todayDate = new Date();
+//   var yesterdayDate = new Date(new Date().getTime() - 1000*60*60*24*1);
+
+//   if (date.getDate() === todayDate.getDate()) {
+//     strTime += 'Today at ' + formatAMPM(date);
+//   } else if (date.getDate() === yesterdayDate.getDate()) {
+//     strTime += 'Yesterday at ' + formatAMPM(date);
+//   } else {
+//     strTime += date.toDateString().slice(4,11) + formatAMPM(date);
+//   }
+//   return strTime;
+// }
+
+// // Display Feeds
+
+// function createFeed(queryjob) {
+//   var feed = '';
+//   feed += '<div style="padding-top: 30px" class="row">';
+//   feed += '<p class="col-sm-8">';
+//   feed += '<span style="display: inline-block; font-weight: bold; padding-right: 10px">' + queryjob.user_name + '&nbsp;';
+//   feed += '<span style="font-weight: normal; color: gray;">' + formatTodayYesterday(new Date(queryjob.timeissued)) + '</span>';
+//   feed += '</span>';
+//   feed += '<span style="display: inline-block;">' + queryjob.typed_cmd + '</span>';
+//   feed += '</p>';
+//   feed += '<p style="text-align: right;" class="col-sm-4">';
+//   feed += '<button type="button" disabled="disabled" style="opacity: 1;" class="btn btn-default btn-xs">deadline at ' + formatAMPM(new Date(queryjob.deadline)) + '</button>';
+//   feed += '&nbsp;&nbsp;';
+//   feed += '<button type="button" disabled="disabled" style="opacity: 1;" class="btn btn-default btn-xs">in queue</button>';
+//   feed += '</p>';
+//   feed += '<div style="text-align: left;" class="col-xs-12">';
+//   feed += '<button type="button" class="btn btn-default">Cancel</button>';
+//   feed += '</div>';
+//   feed += '</div>';
+//   return feed;
+// }
+
+// Submit question.
 function submitQuestion(event) {
   event.preventDefault();
 
-  // TODO(mjyc): implement input validation.
+  // TODO: implement input validation.
 
   var newQueryJob = {
     'timeissued': new Date(),
     'typed_cmd': $('#submitQuestion input#inputTypedCmd').val(),
-    'notification_sms': $('#submitQuestion button#btnToggleSMS').hasClass('active'),
-    'notification_email': $('#submitQuestion button#btnToggleEmail').hasClass('active'),
+    'notification_sms': $('#submitQuestion button#btnToggleSMS')
+      .hasClass('active'),
+    'notification_email': $('#submitQuestion button#btnToggleEmail')
+      .hasClass('active'),
+    'deadline': new Date(new Date().getTime() + 1000*60*60*1*1)
+      .toISOString()
   };
 
   // Use AJAX to post the object to our adduser service.
@@ -134,6 +177,7 @@ function submitQuestion(event) {
     dataType: 'JSON'
   }).done(function(res) {
     console.log(res);
+    // populateFeed(qFeedPage);
   }).fail(function() {
     console.log('error occured');
   });
