a20a026095a50e7cf2922070cff3066c47558025
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-12-02 21:52:19 -0800

    first angular functionality check

--- public/app/controllers/home-controller.js
+++ public/app/controllers/home-controller.js
@@ -1,4 +1,22 @@
 'use strict';
 
-angular.module('askdubeApp').controller('HomeController', ['$scope', function($scope) {
-}]);
+angular.module('askdubeApp').controller('HomeController', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('queryjobs/list').success(function(data) {
+      $scope.queryjobs = data;
+      // console.log(data);
+    });
+  }]);
+
+
+// phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+//   function($scope, $http) {
+//     $http.get('phones/phones.json').success(function(data) {
+//       $scope.phones = data;
+//     });
+
+// phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+//   function($scope, Phone) {
+//     $scope.phones = Phone.query();
+//      $scope.orderProp = 'age';
+//    }]);
