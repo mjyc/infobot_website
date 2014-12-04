0e80aab453c8f02ee8418d7c0e3b5cee35aad055
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-12-03 19:32:40 -0800

    cleaning up home page

--- public/app/controllers/home-controller.js
+++ public/app/controllers/home-controller.js
@@ -4,19 +4,8 @@ angular.module('askdubeApp').controller('HomeController', ['$scope', '$http',
   function($scope, $http) {
     $http.get('queryjobs/list').success(function(data) {
       $scope.queryjobs = data;
-      // console.log(data);
+    });
+    $http.get('users/current').success(function(data) {
+      $scope.user = data;
     });
   }]);
-
-
-// phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-//   function($scope, $http) {
-//     $http.get('phones/phones.json').success(function(data) {
-//       $scope.phones = data;
-//     });
-
-// phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-//   function($scope, Phone) {
-//     $scope.phones = Phone.query();
-//      $scope.orderProp = 'age';
-//    }]);
